72d4237b1cded7aec5377b66eed5d69f2a3fb82a
Divya <medam.divya@capgemini.com>
2023-03-14 16:53:13 +0530

    add all my files

---
+++ OnlineMedicalShopping-1/src/main/java/com/medical/OnlineMedicalShopping1Application.java
@@ -0,0 +1,13 @@
+package com.medical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMedicalShopping1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMedicalShopping1Application.class, args);
+	}
+
+}
---
+++ OnlineMedicalShopping-1/src/main/java/com/medical/repository/OrderRepository.java
@@ -0,0 +1,19 @@
+
+package com.medical.repository;
+
+
+	import org.springframework.stereotype.Repository;
+
+	import com.medical.entity.Order;
+
+	import org.springframework.data.jpa.repository.JpaRepository;
+
+	@Repository
+
+	public interface OrderRepository extends JpaRepository<Order, Integer>  {
+
+	}
+
+
+
+
